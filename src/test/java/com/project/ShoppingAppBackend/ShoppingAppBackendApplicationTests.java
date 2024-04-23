ebae80cbc100253111e3755b8c443116e73dea0c
Bhanover <127310131+Bhanover@users.noreply.github.com>
2024-04-23 16:52:37 +0200

    Creando el proyecto

---
+++ src/test/java/com/project/ShoppingAppBackend/ShoppingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ShoppingAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
